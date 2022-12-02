d153eeb0907a45a69faacfade51a9c9a6c3f84e9
Zimin Zhang <zhang110297@gmail.com>
2022-12-01 21:02:44 -0800

    setup scene

--- AimChallenge.js
+++ AimChallenge.js
@@ -7,6 +7,11 @@ import { Shape_From_File } from './examples/obj-file-demo.js';
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
+const {Cube, Axis_Arrows, Textured_Phong} = defs
+const Z_MAX = 200;
+const Z_MIN = 50;
+const X_MAX = 46;
+const X_MIN = -46;
 
 export class AimChallenge extends Scene {
     constructor() {
@@ -16,27 +21,40 @@ export class AimChallenge extends Scene {
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
             sun: new defs.Subdivision_Sphere(4),
-            wall_obj: new Shape_From_File("assets/wall.obj"),
             floor: new defs.Cube(),
             target: new Shape_From_File("assets/soldier.obj"),
-            rect: new defs.Cube(),
+            side_wall: new defs.Cube(),
+            back_wall: new defs.Cube(),
+            floor: new defs.Cube(),
         };
+
+        this.shapes.side_wall.arrays.texture_coord.forEach((v) => {
+            v[0] = v[0] * 16;
+            v[1] = v[1] * 1;
+        });
+        this.shapes.back_wall.arrays.texture_coord.forEach((v) => {
+            v[0] = v[0] * 8;
+            v[1] = v[1] * 1;
+        });
 
         // *** Materials
         this.materials = {
             sun: new Material(new defs.Phong_Shader(),
                 {ambient: 1, color: hex_color("#ffffff")}),
-            damaged_text: new Material(new defs.Phong_Shader, {
-                ambient: 1, color: hex_color("#808080")}),
             floor: new Material(new defs.Phong_Shader(),
-                {ambient: 0, color: hex_color("#228B22")}),
+                {ambient: 0.3, color: hex_color("#ffffff")}),
             target: new Material(new defs.Phong_Shader(),
-                {ambient: 0.2, color: hex_color("#ff0000")}),
-            wall: new Material(new defs.Phong_Shader(),
-                {ambient: 1, color: hex_color("#ffffff")}),
+                {ambient: 0.5, color: hex_color("#ff0000")}),
+            wall: new Material(new Texture_Scroll_X(),
+                {ambient: 1, diffusivity: 0.5, specularity: 0, texture: new Texture("assets/wall.jpg", "NEAREST")}),
         }
 
-        this.is_targets_down = [false, false, false];
+        // target-related variables
+        this.is_targets_down = [true, true, true, true, true, true, true, true];
+        this.target_list = [];
+        for (let i = 0; i < this.is_targets_down.length; i++) {
+            this.target_list.push(Mat4.identity());
+        }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 1.8, 0), vec3(0, 1.8, -15), vec3(0, 2, -1));
     }
@@ -63,72 +81,58 @@ export class AimChallenge extends Scene {
             Math.PI / 4, context.width / context.height, .1, 1000);
 
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
-        const light_position = vec4(0, 8, 0, 1);
+        const light_position1 = vec4(0, 14, -50, 1);
+        const light_position2 = vec4(0, 14, -150, 1);
         // The parameters of the Light are: position, color, size
-        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 10000)];
-        this.shapes.sun.draw(context, program_state, Mat4.translation(0, 16, 0), this.materials.sun);
+        program_state.lights = [new Light(light_position1, color(1, 1, 1, 1), 10000), new Light(light_position2, color(1, 1, 1, 1), 10000)];
+        this.shapes.sun.draw(context, program_state, Mat4.translation(0, 14, -50), this.materials.sun);
+        this.shapes.sun.draw(context, program_state, Mat4.translation(0, 14, -150), this.materials.sun);
         
         // Draw walls
-        let model_transform_wall_left = Mat4.translation(-11, 0, 0).times(Mat4.scale(0.5, 8, 50)).times(Mat4.translation(1, 1, -1));
-        this.shapes.rect.draw(context, program_state, model_transform_wall_left, this.materials.wall);
-        let model_transform_wall_right = Mat4.translation(10, 0, 0).times(Mat4.scale(0.5, 8, 50)).times(Mat4.translation(1, 1, -1));
-        this.shapes.rect.draw(context, program_state, model_transform_wall_right, this.materials.wall);
-        let model_transform_wall_back = Mat4.translation(-11, 0, -100).times(Mat4.scale(11, 8, 0.5)).times(Mat4.translation(1, 1, -1));
-        this.shapes.rect.draw(context, program_state, model_transform_wall_back, this.materials.wall);
+        let model_transform_wall_left = Mat4.translation(-51, 0, 0).times(Mat4.scale(0.5, 8, 100)).times(Mat4.translation(1, 1, -1));
+        this.shapes.side_wall.draw(context, program_state, model_transform_wall_left, this.materials.wall);
+        let model_transform_wall_right = Mat4.translation(50, 0, 0).times(Mat4.scale(0.5, 8, 100)).times(Mat4.translation(1, 1, -1));
+        this.shapes.side_wall.draw(context, program_state, model_transform_wall_right, this.materials.wall);
+        let model_transform_wall_back = Mat4.translation(-51, 0, -200).times(Mat4.scale(51, 8, 0.5)).times(Mat4.translation(1, 1, -1));
+        this.shapes.back_wall.draw(context, program_state, model_transform_wall_back, this.materials.wall);
 
         // Draw floor and ceiling
-        let model_transform_floor = Mat4.translation(-11, -1, 0).times(Mat4.scale(11, 0.5, 50)).times(Mat4.translation(1, 1, -1));
-        this.shapes.rect.draw(context, program_state, model_transform_floor, this.materials.floor);
-        let model_transform_ceil = Mat4.translation(-11, 16, 0).times(Mat4.scale(11, 0.5, 50)).times(Mat4.translation(1, 1, -1));
-        this.shapes.rect.draw(context, program_state, model_transform_ceil, this.materials.floor);
+        let model_transform_floor = Mat4.translation(-51, -1, 0).times(Mat4.scale(51, 0.5, 100)).times(Mat4.translation(1, 1, -1));
+        this.shapes.floor.draw(context, program_state, model_transform_floor, this.materials.floor);
+        let model_transform_ceil = Mat4.translation(-51, 16, 0).times(Mat4.scale(51, 0.5, 100)).times(Mat4.translation(1, 1, -1));
+        this.shapes.floor.draw(context, program_state, model_transform_ceil, this.materials.floor);
 
         // Setup targets
-        let model_transform_target = (Mat4.translation(0, 0, -1));
 
-        this.shapes.target.draw(context, program_state, model_transform_target, this.materials.target);
-        
-        // let model_transform_wall_front = Mat4.translation(0, 0, -45).times(Mat4.scale(30, 30, 10));
-        // this.shapes.wall_obj.draw(context, program_state, model_transform_wall_front, this.materials.damaged_text);
-        // let model_transform_wall_left = Mat4.translation(-45, 0, 0).times(Mat4.rotation(Math.PI / 2, 0, 1, 0)).times(Mat4.scale(30, 30, 10));
-        // this.shapes.wall_obj.draw(context, program_state, model_transform_wall_left, this.materials.damaged_text);
-        // let model_transform_wall_right = Mat4.translation(45, 0, 0).times(Mat4.rotation(-Math.PI / 2, 0, 1, 0)).times(Mat4.scale(30, 30, 10));
-        // this.shapes.wall_obj.draw(context, program_state, model_transform_wall_right, this.materials.damaged_text);
-        // let model_transform_floor = Mat4.identity().times(Mat4.translation(0, -11, 0)).times(Mat4.scale(30, 0.1, 300));
-        // this.shapes.wall_obj.draw(context, program_state, model_transform_floor, this.materials.floor);
+        for(let i = 0; i < 8; i++){
+            if(this.is_targets_down[i]){
+                let new_x = Math.floor(Math.random() * (X_MAX - X_MIN + 1)) + X_MIN;
+                let new_z = Math.floor(Math.random() * (Z_MAX - Z_MIN + 1)) + Z_MIN;
+                this.target_list[i] = Mat4.translation(new_x, 0, -new_z).times(Mat4.translation(0, 3.25, 0));
+                this.is_targets_down[i] = false;
+            }
+            this.shapes.target.draw(context, program_state, this.target_list[i], this.materials.target);
+        }
+    }
+}
 
-        // let model_transform_target1 = Mat4.identity();
-        // let model_transform_target2 = Mat4.identity();
-        // let model_transform_target3 = Mat4.identity();
-
-        // let model_transform_targets = [model_transform_target1, model_transform_target2, model_transform_target3]
-
-        // for (let i = 0; i < this.is_targets_down.length; i++){
-        //     if (this.is_targets_down[i]){
-        //         model_transform_targets[i] = model_transform_targets[i].times(Mat4.rotation(-Math.PI / 2, 1, 0, 0))
-        //     }
-        // }
-
-        // model_transform_targets[0] = Mat4.translation(-18, -1.25, -15).times(Mat4.scale(3, 3, 3)).times(model_transform_targets[0]);
-        // model_transform_targets[1] = Mat4.translation(18, -1.25, -15).times(Mat4.scale(3, 3, 3)).times(model_transform_targets[1]);
-        // if (this.is_targets_down[2]){
-        //     this.tmp = Mat4.translation(20 * Math.sin(0.7 * t), -1.25, 3 * Math.sin(0.6 * t)).times(Mat4.scale(3, 3, 3)).times(model_transform_targets[2]);
-        //     model_transform_targets[2] = this.tmp;
-        // }
-        // else{
-        //     model_transform_targets[2] = Mat4.translation(20 * Math.sin(0.7 * t), -1.25, 3 * Math.sin(0.6 * t)).times(Mat4.scale(3, 3, 3)).times(model_transform_targets[2]);
-        // }
-
-
-        // for (let i = 0; i < this.is_targets_down.length; i++){
-        //     if (this.is_targets_down[i]){
-        //         model_transform_targets[i] = Mat4.translation(0, -9, -10).times(model_transform_targets[i]);
-        //     }
-        // }
-        
-        
-        // for (let i = 0; i < model_transform_targets.length; i++){
-        //     this.shapes.target.draw(context, program_state, model_transform_targets[i], this.materials.target);
-        // }
-        
+class Texture_Scroll_X extends Textured_Phong {
+    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #6.
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D texture;
+            uniform float animation_time;
+            
+            void main(){
+                vec2 scaled_coord = f_tex_coord - vec2(mod(animation_time * 2.0, 4.0) , 0.0);
+                vec4 tex_color = texture2D( texture, f_tex_coord - vec2(mod(animation_time * 2.0, 4.0) , 0.0));
+                
+                if( tex_color.w < .01 ) discard;  
+                                                                         // Compute an initial (ambient) color:
+                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                                                                         // Compute the final color with contributions from lights:
+                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+        } `;
     }
 }
